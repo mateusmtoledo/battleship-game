9cc4062c8c040fab739f5a76407a6280ca31461f
Mateus Mauricio Toledo <mateusmtoledo1@gmail.com>
2022-07-11 20:27:47 -0300

    Add header

---
+++ src/header.js
@@ -0,0 +1,24 @@
+import githubIcon from './images/github.png';
+
+const header = (() => {
+  const container = document.createElement('header');
+
+  const h1 = document.createElement('h1');
+  h1.textContent = 'battleships';
+
+  const githubLink = document.createElement('a');
+  githubLink.href = 'https://github.com/mateusmtoledo';
+  const githubImg = document.createElement('img');
+  githubImg.src = githubIcon;
+  githubImg.width = 32;
+  githubImg.height = 32;
+  const githubText = document.createElement('p');
+  githubText.textContent = 'mateusmtoledo';
+  githubLink.append(githubImg, githubText);
+
+  container.append(h1, githubLink);
+
+  return container;
+})();
+
+export default header;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import './style.css';
 import newGame from './game';
+import header from './header';
 
+document.body.append(header);
 newGame();
