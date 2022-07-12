c5d94d31354b0beb59ad64cfd2da1176d0759b28
Mateus Mauricio Toledo <mateusmtoledo1@gmail.com>
2022-07-12 15:53:15 -0300

    Add scoreboard

--- src/game.js
+++ src/game.js
@@ -4,7 +4,34 @@ import Player from './Player';
 import Computer from './Computer';
 import pubSub from './pubSub';
 
-const newGame = () => {
+function scoreBoardFactory(player, computer) {
+  const container = document.createElement('div');
+  container.classList.add('scoreboard');
+
+  const playerScoreDiv = document.createElement('div');
+  const playerName = document.createElement('div');
+  playerName.textContent = player.name;
+  const playerScore = document.createElement('p');
+  playerScoreDiv.append(playerName, playerScore);
+
+  const computerScoreDiv = document.createElement('div');
+  const computerName = document.createElement('div');
+  computerName.textContent = computer.name;
+  const computerScore = document.createElement('p');
+  computerScoreDiv.append(computerName, computerScore);
+
+  const update = () => {
+    playerScore.textContent = player.score;
+    computerScore.textContent = computer.score;
+  };
+  update();
+
+  container.append(playerScoreDiv, computerScoreDiv);
+
+  return { container, update };
+}
+
+function newGame() {
   const boardsContainer = document.createElement('div');
   boardsContainer.id = 'boards-container';
   document.body.append(boardsContainer);
@@ -27,14 +54,19 @@ const newGame = () => {
     computerBoard.toggleAttackListener();
     startGameButton.setAttribute('disabled', 'true');
   });
-  document.body.append(startGameButton);
+
+  const scoreBoard = scoreBoardFactory(player, computer);
+
+  document.body.append(startGameButton, scoreBoard.container);
 
   pubSub.subscribe('gameFinished', (sender) => {
     let winner;
     if (sender === playerBoard.gameBoard) winner = computer;
     else winner = player;
+    winner.score += 1;
+    scoreBoard.update(player.score, computer.score);
     console.log(`Winner: ${winner.name}`);
   });
-};
+}
 
 export default newGame;
